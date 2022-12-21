68165f3340bba8b0f90fb60f6ab9966d5ec8d29b
oyinkansola <adebayoalameen88@gmail.com>
2022-12-21 16:35:12 +0100

    Feat: Added the contact componenet Style: Added the styles for the contact page Styele: Modified the style for the home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import logo from "./assets/logo.png";
+import {Link } from "react-router-dom"
 import "./styles/Navbar.scss";
 
 function Navbar() {
   return (
     <nav>
       <div className="left">
-        <img src={logo} alt="logo" />
+        <Link to="/"><img src={logo} alt="logo" /></Link>
         <h3>Hi</h3>
       </div>
       <div className="right">
-        <button>Say Hello</button>
+        <button>
+          <Link className="link" to="/contact">Say Hello</Link>
+        </button>
       </div>
     </nav>
   );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import "../components/styles/Contact.scss";
 
 function Contact() {
   return (
-    <div>
-        <h1>Contact</h1>
+    <div className="contact">
+      <h1>
+        Thanks for taking the time to reach out. How can I help you today?
+      </h1>
+      <form action="">
+        <div className="hold">
+          <div className="a">
+            <label htmlFor="fullName">Name</label>
+            <input type="text" />
+          </div>
+          <div className="a">
+            <label htmlFor="fullName">Email</label>
+            <input type="text" />
+          </div>
+        </div>
+        <div className="message">
+            <label htmlFor="message"> Message</label>
+            <textarea ></textarea>
+          </div>
+          <button type="submit">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
